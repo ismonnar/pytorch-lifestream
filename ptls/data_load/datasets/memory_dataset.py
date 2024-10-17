54c2748ae70455c7dbf1f286181710ac3b7da76c
PvtKaefsky <kaefsky@gmail.com>
2024-10-17 15:27:27 +0300

    Fix seq_len_filter for memory_dataset

--- ptls/data_load/datasets/memory_dataset.py
+++ ptls/data_load/datasets/memory_dataset.py
@@ -1,13 +1,15 @@
 import logging
-from abc import ABC, abstractmethod
 from typing import Iterable, List
+from abc import ABC, abstractmethod
 
 import joblib
+import pandas as pd
 import torch
-from joblib import delayed, Parallel
 from pymonad.either import Either
-
+from joblib import parallel_backend, parallel_config, delayed, Parallel
 from ptls.data_load import IterableChain
+from ptls.data_load.iterable_processing.to_torch_tensor import ToTorch
+from ptls.preprocessing.dask.dask_client import DaskServer
 
 logger = logging.getLogger(__name__)
 
@@ -29,20 +31,16 @@ class MemoryMapDataset(torch.utils.data.Dataset):
                  i_filters: List[Iterable] = None, 
                  n_jobs: int = -1):
         self.n_jobs = n_jobs
-        
-        if i_filters is None:
-            self.processed_data = [rec for rec in data]
-        else:
-            post_processor_filter = IterableChain(*i_filters)
-            self.processed_data = [rec for rec in post_processor_filter(data)]
-        
+        self.processed_data = Either(data, monoid=[i_filters, i_filters is None]).either(
+            left_function=lambda filters: self.__apply_filters(data, filters),
+            right_function=lambda x: [rec for rec in x])
         logger.info(f'Loaded {len(self.processed_data)} records')
 
     def __apply_filters(self, data, i_filters):
         def _iterable_filtration(sample, i_filters):
             for f in i_filters:
                 sample = f(sample)
-            return sample
+            yield sample
 
         with joblib.parallel_backend(backend='threading', n_jobs=self.n_jobs):
             parallel = Parallel(verbose=1)
@@ -62,9 +60,7 @@ class MemoryIterableDataset(torch.utils.data.IterableDataset, ABC):
         super().__init__()
         self.data = data
         self.i_filters = i_filters
-        self.processed_data = Either(data, monoid=[i_filters, i_filters is None]).either(
-            left_function=lambda filters: IterableChain(*filters)(data),
-            right_function=lambda x: [rec for rec in x])
+
     @abstractmethod
     def __iter__(self):
         """This method must be implemented in subclasses"""
