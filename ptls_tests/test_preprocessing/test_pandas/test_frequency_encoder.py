8c9ee07697bed305aed3a4b821e99f803a4bbcab
technocreep <aspirantvadim@gmail.com>
2024-10-24 14:05:15 +0200

    updated unit test imports and action run

--- ptls_tests/test_preprocessing/test_pandas/test_frequency_encoder.py
+++ ptls_tests/test_preprocessing/test_pandas/test_frequency_encoder.py
@@ -1,36 +1,36 @@
 import pandas as pd
-import pytest
 
-from ptls.preprocessing.pandas.pandas_transformation.pandas_freq_transformer import FrequencyEncoder
+# TODO: check if this is the correct import
+from ptls.preprocessing.pyspark.frequency_encoder import FrequencyEncoder
 
 
-@pytest.fixture()
-def get_df_and_encoder():
+def test_fit():
     df = pd.DataFrame({
         'uid': [0, 0, 0, 1, 1, 1, 1, 1],
         'cat': [4, 5, 5, 5, 2, 2, 2, 2],
     })
     t = FrequencyEncoder(col_name_original='cat')
-    return df, t
-
-
-def test_fit(get_df_and_encoder):
-    df, t = get_df_and_encoder
     t.fit(df)
     assert t.mapping == {'2': 1, '5': 2, '4': 3}
     assert t.other_values_code == 4
     assert t.dictionary_size == 5
 
 
-def test_fit_transform(get_df_and_encoder):
-    df, t = get_df_and_encoder
+def test_fit_transform():
+    df = pd.DataFrame({
+        'uid': [0, 0, 0, 1, 1, 1, 1, 1],
+        'cat': [4, 5, 5, 5, 2, 2, 2, 2],
+    })
+    t = FrequencyEncoder(col_name_original='cat')
     out = t.fit_transform(df)
     assert out['cat'].values.tolist() == [3, 2, 2, 2, 1, 1, 1, 1]
 
 
-def test_new(get_df_and_encoder):
-    df_train, t = get_df_and_encoder
-
+def test_new():
+    df_train = pd.DataFrame({
+        'uid': [0, 0, 0, 1, 1, 1, 1, 1],
+        'cat': [4, 5, 5, 5, 2, 2, 2, 2],
+    })
     df_test = pd.DataFrame({
         'uid': [0, 0, 0, 1, 1, 1, 1, 1],
         'cat': [4, 5, 5, 5, 3, 3, 3, 3],
