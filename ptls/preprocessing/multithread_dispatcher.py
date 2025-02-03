a094e5882a8fd7bc1a269aa5de25b8350472a107
technocreep <aspirantvadim@gmail.com>
2025-02-03 11:35:54 +0100

    commit after rebase on main

--- ptls/preprocessing/multithread_dispatcher.py
+++ ptls/preprocessing/multithread_dispatcher.py
@@ -1,25 +1,21 @@
 from typing import Union, List, Dict, Callable
 
-import dask
 import pandas as pd
-from joblib import wrap_non_picklable_objects, parallel_backend
+from joblib import wrap_non_picklable_objects, parallel_backend, Parallel, delayed
 from pymonad.maybe import Maybe
 from ptls.preprocessing.dask.dask_client import DaskServer
 
-from ptls.preprocessing.util import determine_n_jobs
-
 
 class DaskDispatcher:
-    def __init__(self, n_jobs: int = -1):
-        self.dask_client = DaskServer(n_jobs=n_jobs).client
-        print(f"Link Dask Server - {self.dask_client.dashboard_link}")
-        self.transformation_func = "fit_transform"
-        self.n_jobs = determine_n_jobs(-1)
+    def __init__(self, n_jobs: int):
+        self.dask_client = DaskServer(n_jobs).client
+        print(f'Link Dask Server - {self.dask_client.dashboard_link}')
+        self.transformation_func = 'fit_transform'
+        self.n_jobs = n_jobs
 
     def shutdown(self):
-        # self.dask_client.close()
-        # del self.dask_client
-        del self
+        self.dask_client.close()
+        del self.dask_client
 
     def _multithread_eval(
             self,
@@ -52,39 +48,31 @@ class DaskDispatcher:
         #     without dask
 
         if isinstance(objective_func, dict):
-            evaluation_results = [
-                self.evaluate_single(
-                    self,
-                    # data=individuals_to_evaluate[func_name],
-                    data=pd.DataFrame(individuals_to_evaluate[func_name]),
-                    eval_func=func_impl,
-                )
-                for func_name, func_impl in objective_func.items()
-            ]
+            with parallel_backend(backend='dask',
+                                  n_jobs=self.n_jobs,
+                                  scatter=list(individuals_to_evaluate.values())
+                                  ):
+                evaluation_results = [self.evaluate_single(self,
+                                                           data=individuals_to_evaluate[func_name],
+                                                           eval_func=func_impl) for func_name, func_impl in
+                                      objective_func.items()]
         else:
-            evaluation_results = self.evaluate_single(
-                self, data=individuals_to_evaluate, eval_func=objective_func
-            )
+            evaluation_results = self.evaluate_single(self,
+                                                      data=individuals_to_evaluate,
+                                                      eval_func=objective_func)
         return evaluation_results
 
-    def evaluate(
-            self,
-            individuals: Union[List, Dict],
-            objective_func: Union[Callable, List[Callable]],
-    ):
-        individuals_evaluated = Maybe.insert(individuals).maybe(
-            default_value=None,
-            extraction_function=lambda generation: self._multithread_eval(
-                generation, objective_func
-            ),
-        )
+    def evaluate(self, individuals: Union[List, Dict], objective_func: Union[Callable, List[Callable]]):
+        individuals_evaluated = Maybe.insert(individuals). \
+            maybe(default_value=None,
+                  extraction_function=lambda generation: self._multithread_eval(generation, objective_func))
         return individuals_evaluated
 
     @wrap_non_picklable_objects
-    def evaluate_single(
-            self, data: Union[pd.DataFrame, pd.Series], eval_func: Callable
-    ):
-        if self.transformation_func == "fit_transform":
+    def evaluate_single(self,
+                        data: Union[pd.DataFrame, pd.Series],
+                        eval_func: Callable):
+        if self.transformation_func == 'fit_transform':
             eval_res = eval_func.fit_transform(data)
         else:
             eval_res = eval_func.transform(data)
